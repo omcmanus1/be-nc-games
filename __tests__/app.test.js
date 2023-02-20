7a389da4e11637007d318a82b7d97c86d46f11cf
Oliver McManus <ollie_mc@Olivers-MacBook-Pro.local>
2023-02-20 14:18:38 +0000

    refactor jest assertions for GET categories tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -14,33 +14,36 @@ afterAll(() => db.end());
 
 describe("/api/categories", () => {
   test("GET: should respond with a 200 status code", () => {
-    return request(app).get("/api/categories").expect(200);
-  });
-  test("GET: should respond with an array", () => {
     return request(app)
       .get("/api/categories")
+      .expect(200)
       .then(({ body }) => {
         const { categories } = body;
         expect(categories).toBeInstanceOf(Array);
-      });
-  });
-  test("GET: should respond with an array of the correct length", () => {
-    return request(app)
-      .get("/api/categories")
-      .then(({ body }) => {
-        const { categories } = body;
         expect(categories.length).toBe(categoryData.length);
-      });
-  });
-  test("GET: should respond with an array of category objects with the correct keys", () => {
-    return request(app)
-      .get("/api/categories")
-      .then(({ body }) => {
-        const { categories } = body;
         categories.forEach((category) => {
           expect(category).toHaveProperty("slug");
           expect(category).toHaveProperty("description");
         });
+        // test("GET: should respond with an array", () => {
+        //   const { categories } = body;
+        //   expect(categories).toBeInstanceOf(Array);
+        // });
+        // test("GET: should respond with an array of the correct length", () => {
+        //   const { categories } = body;
+        //   expect(categories.length).toBe(categoryData.length);
+        // });
+        // test("GET: should respond with an array of category objects with the correct keys", () => {
+        //   return request(app)
+        //     .get("/api/categories")
+        //     .then(({ body }) => {
+        //       const { categories } = body;
+        //       categories.forEach((category) => {
+        //         expect(category).toHaveProperty("slug");
+        //         expect(category).toHaveProperty("description");
+        //       });
+        //     });
+        // });
       });
   });
   test("GET: should respond with a 400 error if incorrect endpoint is specified", () => {
