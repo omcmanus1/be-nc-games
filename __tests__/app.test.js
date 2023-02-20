e76baba0cd0fa1b0e4dae3003ffa57400300649b
Oliver McManus <ollie_mc@Olivers-MacBook-Pro.local>
2023-02-20 12:24:48 +0000

    setup app test file + initial tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,4 +16,34 @@ describe("/api/categories", () => {
   test("GET: should respond with a 200 status code", () => {
     return request(app).get("/api/categories").expect(200);
   });
+  test("GET: should respond with an array", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then(({ body }) => {
+        const { categories } = body;
+        expect(categories).toBeInstanceOf(Array);
+      });
+  });
+  test("GET: should respond with an array of the correct length", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then(({ body }) => {
+        const { categories } = body;
+        expect(categories.length).toBe(categoryData.length);
+      });
+  });
+  test("GET: should respond with an array of category objects with the correct keys", () => {
+    return request(app)
+      .get("/api/categories")
+      .expect(200)
+      .then(({ body }) => {
+        const { categories } = body;
+        categories.forEach((category) => {
+          expect(category).toHaveProperty("slug");
+          expect(category).toHaveProperty("description");
+        });
+      });
+  });
 });
