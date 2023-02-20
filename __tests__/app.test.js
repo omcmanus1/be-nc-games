adb7b83c94edcc8795d4ad6bf7808227007345b7
Oliver McManus <ollie_mc@Olivers-MacBook-Pro.local>
2023-02-20 22:40:27 +0000

    GET /api/reviews/:id tests (+ invalid ID error handling)

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -32,13 +32,36 @@ describe("/api/categories", () => {
   });
 });
 
-describe.skip("/api/reviews/:review_id", () => {
-  test("GET: should respond with a 200 status code and expected single review", () => {
+describe.only("/api/reviews/:id", () => {
+  test("GET: should respond with an array containing correct single review object", () => {
     return request(app)
-      .get("/api/reviews/1")
+      .get("/api/reviews/2")
       .expect(200)
       .then(({ body }) => {
         const { review } = body;
+        expect(review).toBeInstanceOf(Array);
+        expect(review.length).toBe(1);
+        const reviewTemplate = {
+          review_id: expect.any(Number),
+          title: expect.any(String),
+          review_body: expect.any(String),
+          designer: expect.any(String),
+          review_img_url: expect.any(String),
+          votes: expect.any(Number),
+          category: expect.any(String),
+          owner: expect.any(String),
+          created_at: expect.any(String),
+        };
+        expect(review[0]).toMatchObject(reviewTemplate);
+        expect(review[0].review_id).toBe(2);
+      });
+  });
+  test("GET: should respond with 400 if qeuried with invalid ID", () => {
+    return request(app)
+      .get("/api/reviews/56777")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.msg).toBe("Not Found");
       });
   });
 });
