d2ddfa9819438a3c110c22c406be53910b535f66
Oliver McManus <ollie_mc@Olivers-MacBook-Pro.local>
2023-02-20 15:50:06 +0000

    implement basic /api/reviews GET query

--- models/reviews-models.js
+++ models/reviews-models.js
@@ -1,3 +1,15 @@
 const db = require("../db/connection");
 
-exports.selectReviews = () => {};
+exports.selectReviews = () => {
+  const queryString = `
+  SELECT reviews.owner, reviews.title, reviews.review_id, reviews.category,
+    reviews.review_img_url, reviews.created_at, reviews.votes, 
+    reviews.designer, COUNT(comments.body) AS comment_count
+  FROM reviews
+  INNER JOIN comments
+  ON reviews.review_id = comments.review_id
+  GROUP BY reviews.review_id
+  ORDER BY reviews.created_at
+  `;
+  return db.query(queryString).then((reviews) => reviews.rows);
+};
