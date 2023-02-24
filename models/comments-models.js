195ca57d32e3aafc9e02b1c7fad85d05cafc3834
Oliver McManus <ollie_mc@Olivers-MacBook-Pro.local>
2023-02-24 16:59:21 +0000

    refactor utils folders, add to endpoints file

--- models/comments-models.js
+++ models/comments-models.js
@@ -1,5 +1,5 @@
 const db = require("../db/connection");
-const { checkIdExists } = require("./utils");
+const { checkIdExists } = require("../utils/utils");
 
 exports.insertSingleComment = (commentObj, reviewId) => {
   const requiredProperties = ["username", "body"];
