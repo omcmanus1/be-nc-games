a3c3bac08c1070088c395792aaa9140ca2153138
Oliver McManus <ollie_mc@Olivers-MacBook-Pro.local>
2023-03-01 18:56:06 +0000

    GET user by username logic + basic test

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,7 +1,16 @@
-const { selectUsers } = require("../models/users-models");
+const { selectUsers, selectUserByUsername } = require("../models/users-models");
 
 exports.getUsers = (req, res, next) => {
   selectUsers()
     .then((users) => res.status(200).send({ users }))
+    .catch((err) => next(err));
+};
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params;
+  selectUserByUsername(username)
+    .then((user) => {
+      res.status(200).send({ user });
+    })
     .catch((err) => next(err));
 };
--- models/users-models.js
+++ models/users-models.js
@@ -4,3 +4,12 @@ exports.selectUsers = () => {
   const queryString = `SELECT * FROM users`;
   return db.query(queryString).then((users) => users.rows);
 };
+
+exports.selectUserByUsername = (username) => {
+  const queryString = `
+    SELECT * FROM USERS
+    WHERE username = $1`;
+  return db.query(queryString, [username]).then((user) => {
+    return user.rows;
+  });
+};
